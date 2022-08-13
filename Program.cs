14b42fab999b42634733e36e5376afcd8cb82f4a
Kristina <krislady1412@gmail.com>
2022-08-13 10:41:58 +0600

    Task solution

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿string[] text = { "hello", "wld", "2", ":-)", "car" };
+
+string result = String.Empty;
+string word = String.Empty;
+int neededlength = 3;
+for (int i = 0; i < text.Length; i++)
+{
+    word = text[i];
+    if (word.Length == neededlength)
+    {
+        result = result + " " + text[i];
+    }
+}
+
+string[] array = result.Split(" ");
+Console.WriteLine();
+
+void FillArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+FillArray(array); 
